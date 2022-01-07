41b85fccfa97a88978f311ce81d4ab77a1f4d85d
clara <10088152+claraj@users.noreply.github.com>
2022-01-07 08:53:03 -0600

    notes on also function, some tidying

--- AuroraForecast/app/src/main/java/com/example/auroraforecast/App.kt
+++ AuroraForecast/app/src/main/java/com/example/auroraforecast/App.kt
@@ -8,6 +8,15 @@ private const val TAG = "AURORA APPLICATION"
 
 class App: Application() {
 
+    // The Android system instantiates an instance of this class before any activities,
+    // so you can put initialization code here if needed, for example, in onCreate
+    // You need to list this class in the manifest
+    // This class has a reference to the app context so storing a static reference to
+    // the context can be useful if other non-Activity parts of your code need a reference
+    // to the app context.   The compiler will display a warning - we don't want to keep
+    // static references to the app context in general since these stick around and use
+    // memory, but one here is ok.
+
     companion object {
         lateinit var context: Context
     }
--- AuroraForecast/app/src/main/java/com/example/auroraforecast/AuroraViewModel.kt
+++ AuroraForecast/app/src/main/java/com/example/auroraforecast/AuroraViewModel.kt
@@ -15,10 +15,26 @@ class AuroraViewModel: ViewModel() {
     val reports: MutableLiveData<ReportWrapper> by lazy {
         // The also scope function is passed the new MutableLiveData as an
         // argument, and returns that MutableLiveData object.
+        // A convenient way to perform operations on an object.
         MutableLiveData<ReportWrapper>().also {
             fetchReports()
         }
     }
+
+
+    // Compare to the version without also. The MutableLiveData must be initialized
+    // before the fetchReports function is called, since fetchReports updates the
+    // MutableLive data. This is the more "classic" version of the above.
+//        val reports: MutableLiveData<ReportWrapper> by lazy {
+//            val mutableLiveData =  MutableLiveData<ReportWrapper>()
+//            fetchReports()
+//            // If the inferred return type of the lambda is not Unit,
+//            // the last (or possibly single) expression inside the lambda body
+//            // is treated as the return value. https://kotlinlang.org/docs/lambdas.html#lambda-expression-syntax
+//            mutableLiveData  // the return value
+//        }
+
+
 
     private fun fetchReports() {
         ForecastApiRequest().requestAurora(
