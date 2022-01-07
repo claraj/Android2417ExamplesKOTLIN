f93afc5f10e954ee5eef732fd57da261c62175a8
clara <10088152+claraj@users.noreply.github.com>
2022-01-07 17:58:40 -0600

    seekbar landscape and portrait

--- SeekbarPortraitLandscape/app/src/main/java/com/example/seekbarportraitlandscape/MainActivity.kt
+++ SeekbarPortraitLandscape/app/src/main/java/com/example/seekbarportraitlandscape/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.seekbarportraitlandscape
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var seekbar: SeekBar
+    private lateinit var seekbarLabel: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        seekbar = findViewById(R.id.seek_bar)
+        seekbarLabel = findViewById(R.id.seek_bar_label)
+
+        val initialProgress = seekbar.progress
+        updateLabel(initialProgress)
+
+        seekbar.setOnSeekBarChangeListener( object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekbar: SeekBar?, progress: Int, fromUser: Boolean) {
+                updateLabel(progress)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+    }
+
+    fun updateLabel(progress: Int) {
+        seekbarLabel.text = getString(R.string.seekbar_value_message, progress)
     }
 }
+
