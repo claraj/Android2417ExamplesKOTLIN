dd819741af2ce3d3ddd694e7401ca7ea7e291fc6
clara <10088152+claraj@users.noreply.github.com>
2022-01-17 10:11:35 -0600

    use device min width

--- RedBlueFragment_5_SmallScreen_LargeScreen/app/src/main/java/com/example/redbluefragment/MainActivity.kt
+++ RedBlueFragment_5_SmallScreen_LargeScreen/app/src/main/java/com/example/redbluefragment/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.redbluefragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.FrameLayout
 import androidx.lifecycle.ViewModelProvider
+import androidx.window.layout.WindowMetricsCalculator
+
 
 const val RANDOM_NUMBER_GENERATED = "com.example.redbluefragment.RANDOM_NUMBER_GENERATED"
+
+enum class WindowWidth { SMALL, LARGE }
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,10 +18,32 @@ class MainActivity : AppCompatActivity() {
         ViewModelProvider(this).get(RandomNumberViewModel::class.java)
     }
 
+    // Assume phone, change if needed
+    private var screenWidth: WindowWidth = WindowWidth.SMALL
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val deviceSmallestWidth = resources.configuration.smallestScreenWidthDp
+        Log.d("MAIN_ACTIVITY", "Device smallest width $deviceSmallestWidth")
+
+        screenWidth = if (deviceSmallestWidth < 600) {
+            WindowWidth.SMALL
+        } else {
+            WindowWidth.LARGE
+        }
+
+        Log.d("MAIN_ACTIVITY", "Screen width $screenWidth")
+
+        if (screenWidth == WindowWidth.SMALL) {
+            displaySwappableFragments()
+        } else {
+            displayBothFragments()
+        }
+    }
+
+    private fun displaySwappableFragments() {
         // Display the red fragment when the app loads
         val redFragment = RedFragment.newInstance()
         supportFragmentManager.beginTransaction()
@@ -45,9 +70,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun displayBothFragments() {
+
+        val redFragment = RedFragment.newInstance()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.red_fragment_container, redFragment, "RED")
+            .commit()
+
+        val blueFragment = BlueFragment.newInstance()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.blue_fragment_container, blueFragment, "BLUE")
+            .commit()
+
+    }
+
     override fun onBackPressed() {
         super.onBackPressed()
-        randomNumberViewModel.showBlueFragment = false
+
+        if (screenWidth == WindowWidth.SMALL) {
+            randomNumberViewModel.showBlueFragment = false
+        }
     }
 }
 
