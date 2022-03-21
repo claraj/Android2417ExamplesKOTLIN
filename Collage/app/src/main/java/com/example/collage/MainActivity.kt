320fbf17db522de3e58b8569b5ee1333a08a163f
clara <10088152+claraj@users.noreply.github.com>
2022-03-20 21:09:03 -0500

    fix variable names to match videos, only show save button for Q and beyond

--- Collage/app/src/main/java/com/example/collage/MainActivity.kt
+++ Collage/app/src/main/java/com/example/collage/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.collage
 
 
-import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
@@ -18,21 +17,16 @@ import android.widget.ImageButton
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.FileProvider
-import androidx.lifecycle.coroutineScope
-import androidx.lifecycle.lifecycleScope
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
-import kotlinx.coroutines.*
 import java.io.File
-import java.io.FileOutputStream
 import java.io.IOException
 import java.io.OutputStream
 import java.lang.Exception
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.coroutines.suspendCoroutine
 
 const val TAG = "MAIN_ACTIVITY"
 
@@ -41,15 +35,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mainView: View
     private lateinit var saveButton: FloatingActionButton
     private lateinit var imageButtons: List<ImageButton>
+
     private var photoPaths: ArrayList<String?> = arrayListOf(null, null, null, null)
 
     private var whichImageIndex: Int? = null
 
-    private var newPhotoPath: String? = null
+    private var currentPhotoPath: String? = null
 
-    private val PHOTO_LIST_ARRAY_KEY = "path list key"
+    private val PHOTO_PATH_LIST_ARRAY_KEY = "photo path list key"
     private val IMAGE_INDEX_KEY = "image index key"
-    private val NEW_PHOTO_PATH_KEY = "new photo path key"
+    private val CURRENT_PHOTO_PATH_KEY = "current photo path key"
 
 
     private val cameraActivityLauncher =
@@ -63,8 +58,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         whichImageIndex = savedInstanceState?.getInt(IMAGE_INDEX_KEY) ?: -1
-        newPhotoPath = savedInstanceState?.getString(NEW_PHOTO_PATH_KEY)
-        photoPaths = savedInstanceState?.getStringArrayList(PHOTO_LIST_ARRAY_KEY) ?: arrayListOf(
+        currentPhotoPath = savedInstanceState?.getString(CURRENT_PHOTO_PATH_KEY)
+        photoPaths = savedInstanceState?.getStringArrayList(PHOTO_PATH_LIST_ARRAY_KEY) ?: arrayListOf(
             null,
             null,
             null,
@@ -75,18 +70,24 @@ class MainActivity : AppCompatActivity() {
 
         saveButton = findViewById(R.id.saveFloatingActionButton)
 
+        // the save collage code doesn't work on earlier versions of Android,
+        // so only show the save button on Q or newer
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {  // Q  AKA Android 10 or newer
+            saveButton.visibility = View.VISIBLE
+        }
+
         saveButton.setOnClickListener {
             saveCollageToGallery()
         }
 
-        imageButtons = listOf(
+        imageButtons = listOf<ImageButton>(
             findViewById(R.id.imageButton1),
             findViewById(R.id.imageButton2),
             findViewById(R.id.imageButton3),
             findViewById(R.id.imageButton4)
         )
 
-        for (imageButton: ImageButton in imageButtons) {
+        for (imageButton in imageButtons) {
             imageButton.setOnClickListener { ib ->
                 takePictureFor(ib as ImageButton)
             }
@@ -96,8 +97,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putStringArrayList(PHOTO_LIST_ARRAY_KEY, photoPaths)
-        outState.putString(NEW_PHOTO_PATH_KEY, newPhotoPath)
+        outState.putStringArrayList(PHOTO_PATH_LIST_ARRAY_KEY, photoPaths)
+        outState.putString(CURRENT_PHOTO_PATH_KEY, currentPhotoPath)
         whichImageIndex?.let { outState.putInt(IMAGE_INDEX_KEY, it) }
     }
 
@@ -123,9 +124,8 @@ class MainActivity : AppCompatActivity() {
 
         val (photoFile, photoPath) = createImageFile()
         if (photoFile != null) {
-            newPhotoPath = photoPath
-            val photoUri =
-                FileProvider.getUriForFile(this, "com.example.collage.fileprovider", photoFile)
+            currentPhotoPath = photoPath
+            val photoUri = FileProvider.getUriForFile(this, "com.example.collage.fileprovider", photoFile)
             takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
             cameraActivityLauncher.launch(takePictureIntent)
         }
@@ -136,9 +136,9 @@ class MainActivity : AppCompatActivity() {
 
         when (result.resultCode) {
             RESULT_OK -> {
-                Log.d(TAG, "Result ok, image at $newPhotoPath")
+                Log.d(TAG, "Result ok, image at $currentPhotoPath")
                 // overwrite the index with the current file in preparation for loading new image
-                whichImageIndex?.let { photoPaths[it] = newPhotoPath }
+                whichImageIndex?.let { index -> photoPaths[index] = currentPhotoPath }
             }
             RESULT_CANCELED -> {
                 Log.d(TAG, "Result cancelled, no picture taken")
@@ -181,7 +181,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun saveCollageToGallery() {
         // TODO use a coroutine so not on main thread. Include lifecycle dependency in build.gradle
-        if (photoPaths.all { it != null }) {
+        if (photoPaths.all { it != null }) {  // if all the pictures are taken,
             val bitmap =
                 Bitmap.createBitmap(mainView.width, mainView.height, Bitmap.Config.ARGB_8888)
             val canvas = Canvas(bitmap)
